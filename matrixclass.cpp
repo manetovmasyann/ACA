18f4ebfcea88804da01195929f59d9c1d6bd18c1
manetovmasyann <manetovmasyan.06@gmail.com>
2025-10-20 19:17:23 +0400

    Matrixclass

--- matrixclass.cpp
+++ matrixclass.cpp
@@ -9,10 +9,14 @@ Matrix(int rows ,int cols)
   y=cols;
   data = new int*[x];
  for(int i = 0; i < x ;i++)
- { data[i]=new int[y];
+ { 
+    data[i]=new int[y];
  for(int j = 0 ; j < y ;j++)
- { data[i][j] =  1;
-}}}
+{
+     data[i][j] =  1;
+}
+  }
+   }
 void print()
 {
   for(int i = 0;i < x ;i++)
@@ -21,17 +25,17 @@ void print()
     std::cout<<data[i][j];
     }
   std::cout<<std::endl;
-}
+  }
 }
  ~Matrix()
 {
- for(int i= 0 ; i < x ;i++)
+  for(int i= 0 ; i < x ;i++)
   delete[] data[i];
   delete[] data;
-}
+ }
 };
 int main()
 {  Matrix m(3,4);
- m.print();
- return 0;
+   m.print();
+   return 0;
 }
